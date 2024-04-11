37ff8d807908608b2dab6445a08eaeef87b3d5d0
Suresh Attaluri <suresh@MacBook-Pro.local>
2024-04-11 13:47:07 +0530

    use buf proto validations instead of custom options

--- zzjava/src/test/java/buf/gen/cloud/planton/apis/code2cloud/v1/kafkacluster/rpc/KafkaClusterTest.java
+++ zzjava/src/test/java/buf/gen/cloud/planton/apis/code2cloud/v1/kafkacluster/rpc/KafkaClusterTest.java
@@ -1,6 +1,7 @@
 package buf.gen.cloud.planton.apis.code2cloud.v1.kafkacluster.rpc;
 
-import build.buf.gen.cloud.planton.apis.code2cloud.v1.kafkacluster.model.KafkaCluster;
+import build.buf.gen.cloud.planton.apis.code2cloud.v1.kafkacluster.model.*;
+import build.buf.gen.cloud.planton.apis.code2cloud.v1.kubecluster.model.ContainerResources;
 import build.buf.gen.cloud.planton.apis.commons.apiresource.model.ApiResourceMetadata;
 import build.buf.gen.cloud.planton.apis.commons.apiresource.model.ApiResourceMetadataVersion;
 import build.buf.protovalidate.Validator;
@@ -101,6 +102,91 @@ public final class KafkaClusterTest {
                 .filter(violation -> violation.getMessage().equals("Name must be between 1 and 12 characters long")).findFirst();
         assertFalse(versionMessageViolation.isPresent());
     }
+
+    @Test
+    public void testKafkaCluster_ShouldReturnValidationErrorIfContainerReplicasAndDiskSizeIsNotSet() throws ValidationException {
+        var kafkaCluster = KafkaCluster.newBuilder()
+                .setMetadata(ApiResourceMetadata.newBuilder()
+                        .setName("test")
+                        .setVersion(ApiResourceMetadataVersion.newBuilder().setMessage(" test kafka cluster").build())
+                        .build())
+                .setSpec(KafkaClusterSpec.newBuilder()
+                        .setKubernetes(KafkaClusterSpecKubernetesSpec.newBuilder()
+                                .setZookeeperContainer(KafkaClusterSpecKubernetesSpecZookeeperContainerSpec.newBuilder().build())
+                                .setKafkaBrokerContainer(KafkaClusterSpecKubernetesSpecKafkaBrokerContainerSpec.newBuilder().build())
+                                .build())
+                        .build())
+                .build();
+        Validator validator = new Validator();
+        var result = validator.validate(kafkaCluster);
+        assertTrue(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.kafka_broker_container.replicas"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+        assertTrue(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.kafka_broker_container.disk_size"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+        assertTrue(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.kafka_broker_container.resources"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+
+        assertTrue(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.zookeeper_container.replicas"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+        assertTrue(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.zookeeper_container.disk_size"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+        assertTrue(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.zookeeper_container.resources"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+    }
+
+
+    @Test
+    public void testKafkaCluster_ShouldNotReturnValidationErrorIfContainerReplicasAndDiskSizeIsSet() throws ValidationException {
+        var kafkaCluster = KafkaCluster.newBuilder()
+                .setMetadata(ApiResourceMetadata.newBuilder()
+                        .setName("test")
+                        .setVersion(ApiResourceMetadataVersion.newBuilder().setMessage(" test kafka cluster").build())
+                        .build())
+                .setSpec(KafkaClusterSpec.newBuilder()
+                        .setKubernetes(KafkaClusterSpecKubernetesSpec.newBuilder()
+                                .setZookeeperContainer(KafkaClusterSpecKubernetesSpecZookeeperContainerSpec.newBuilder()
+                                        .setDiskSize("8Gi")
+                                        .setReplicas(1)
+                                        .setResources(ContainerResources.newBuilder().build())
+                                        .build())
+                                .setKafkaBrokerContainer(KafkaClusterSpecKubernetesSpecKafkaBrokerContainerSpec.newBuilder()
+                                        .setDiskSize("8Gi")
+                                        .setReplicas(1)
+                                        .setResources(ContainerResources.newBuilder().build())
+                                        .build())
+                                .build())
+                        .build())
+                .build();
+        Validator validator = new Validator();
+        var result = validator.validate(kafkaCluster);
+        assertFalse(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.kafka_broker_container.replicas"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+        assertFalse(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.kafka_broker_container.disk_size"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+        assertFalse(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.kafka_broker_container.resources"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+
+        assertFalse(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.zookeeper_container.replicas"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+        assertFalse(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.zookeeper_container.disk_size"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+        assertFalse(result.getViolations().stream()
+                .filter(violation -> violation.getFieldPath().equals("spec.kubernetes.zookeeper_container.resources"))
+                .filter(violation -> violation.getMessage().equals("value is required")).findFirst().isPresent());
+    }
+
+
 
 }
 
