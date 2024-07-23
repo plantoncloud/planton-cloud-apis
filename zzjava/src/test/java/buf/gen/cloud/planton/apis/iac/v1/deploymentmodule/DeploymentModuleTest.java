5db791f416ac19fae04875c54189be9b4675aee7
sureshattaluri <suresh.attaluri789@gmail.com>
2024-07-23 13:03:40 +0530

    modify deployment module spec

--- zzjava/src/test/java/buf/gen/cloud/planton/apis/iac/v1/deploymentmodule/DeploymentModuleTest.java
+++ zzjava/src/test/java/buf/gen/cloud/planton/apis/iac/v1/deploymentmodule/DeploymentModuleTest.java
@@ -1,16 +1,16 @@
-package buf.gen.cloud.planton.apis.iac.v1.stackmoduledescriptor;
+package buf.gen.cloud.planton.apis.iac.v1.deploymentmodule;
 
-import build.buf.gen.cloud.planton.apis.iac.v1.stackmoduledescriptor.model.StackModuleDescriptor;
+import build.buf.gen.cloud.planton.apis.iac.v1.deploymentmodule.model.DeploymentModule;
 import build.buf.protovalidate.Validator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-public final class StackModuleDescriptorTest {
+public final class DeploymentModuleTest {
 
     @Test
     public void test_ShouldNotThroughProtoValidationException() {
-        var input = StackModuleDescriptor.newBuilder().build();
+        var input = DeploymentModule.newBuilder().build();
         Validator validator = new Validator();
         assertDoesNotThrow(() -> validator.validate(input));
     }
